84a21e8081730173ee74105a4fa1eda858b9cf2e
BenjaminThomas <haotian19981998@gmail.com>
2022-01-13 15:20:47 +0800

    master

--- src/views/help/index.vue
+++ src/views/help/index.vue
@@ -21,6 +21,58 @@
                 </el-button>
                 获取完整代码
             </span>
+            <p>fip-shellwe-admin是基于多个后台管理系统糅合更改进行从新开发的框架整体主题风格采用 ElementUI,若不熟悉可先去了解。</p>
+            <el-collapse v-model="activeNames" @change="handleChange">
+                <el-collapse-item title="1.1环境搭建" name="1">
+                    <div>前端项目(指代fip-shellwe-admin下同),采用vue.js编写所以需要搭建对应的环境。</div>
+                    <div><a :href="'https://www.cnblogs.com/lgx5/p/10732016.html'">安装教程 - 每天进步多一点 - 博客园</a> 此链接包含node.js安装 vue vue-cli等必须的开发环境安装说明,在此就不再过多赘述。</div>
+                </el-collapse-item>
+                <el-collapse-item title="1.2运行此项目" name="2">
+                    <el-collapse-item title="1.2.1 命令提示符形式(推荐)" name="2-sub1">
+                        <div>
+                            你应该已经拥有了本项目压缩包，由于内部文件仍在修改，所以不提供哈希值验证，直接解压进入目录即可<br>
+                            <img :src="img121"><br>
+                            <img :src="img1211"><br>
+                            进入此页面，在vue所需环境安装完成后输入<br>
+                            <p>
+                                # 安装依赖<br>
+                                npm install<br>
+                                # 运行 其中变量为dev(开发环境) 建议使用 serve(生产环境) 或init<br>
+                                npm run serve<br>
+                            </p>
+                            <img :src="img1212"><br>
+                            弹出此窗口会触发默认浏览器自动打开，建议使用谷歌浏览器，若没有触发，可自行到浏览器输入
+                            localhost:8089 此处可点击触发<br>
+                        </div>
+                    </el-collapse-item>
+                    <el-collapse-item title="1.2.2 IDEA形式(开发)" name="2-sub2">
+                        <div>
+                            <img :src="img1221"><br>
+                            使用IDEA打开存放项目的路径<br>
+                            打开后如图<br>
+                            <img :src="img1222"><br>
+                            <img :src="img1223"><br>
+                            <img :src="img1224"><br>
+                            <img :src="img1225"><br>
+                            选择npm<br>
+                            <img :src="img1226"><br>
+                            <img :src="img1227"><br>
+                            点击运行<br>
+                            <img :src="img1228"><br>
+                            Done 说明运行成功会触发默认浏览器访问,或者点击上方连接<br>
+                            <img :src="img1229">此为中止按钮，功能同命令提示符中输入 Ctrl+C效果一致<br>
+                        </div>
+                    </el-collapse-item>
+                </el-collapse-item>
+                <el-collapse-item title="开发中" name="3">
+                    <div>简化中</div>
+                    <div>决策中</div>
+                </el-collapse-item>
+                <el-collapse-item title="控制中" name="4">
+                    <div>决策中</div>
+                </el-collapse-item>
+            </el-collapse>
+
             <el-dialog
                 title="来自shellwe的提示"
                 :visible.sync="centerDialogVisible"
@@ -46,8 +98,23 @@ export default {
     components: {PageMain, PageHeader},
     data() {
         return {
-            centerDialogVisible: false
+            img121: require('../../../public/image/121.png'),
+            img1211: require('../../../public/image/1211.png'),
+            img1212: require('../../../public/image/1212.png'),
+            img1221: require('../../../public/image/1221.png'),
+            img1222: require('../../../public/image/1222.png'),
+            img1223: require('../../../public/image/1223.png'),
+            img1224: require('../../../public/image/1224.png'),
+            img1225: require('../../../public/image/1225.png'),
+            img1226: require('../../../public/image/1226.png'),
+            img1227: require('../../../public/image/1227.png'),
+            img1228: require('../../../public/image/1228.png'),
+            img1229: require('../../../public/image/1229.png'),
+            centerDialogVisible: false,
+            activeNames: ['1']
         }
+    },
+    mounted() {
     },
     methods: {
         back() {
@@ -74,6 +141,9 @@ export default {
                     message: '这是一条错误的提示消息'
                 })
             }
+        },
+        handleChange(val) {
+            console.log(val)
         }
     }
 }
