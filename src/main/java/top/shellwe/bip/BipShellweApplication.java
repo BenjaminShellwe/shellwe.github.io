a81e44b027c5c0ed44d44a97e9df4173269efbf8
BenjaminThomas <haotian19981998@gmail.com>
2022-01-22 00:30:43 +0800

    fip-admin backend developed by spring boot

---
+++ src/main/java/top/shellwe/bip/BipShellweApplication.java
@@ -0,0 +1,13 @@
+package top.shellwe.bip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipShellweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BipShellweApplication.class, args);
+    }
+
+}
